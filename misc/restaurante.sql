4acca5e35d9701df9e77f173afb9822608d46f55
christian-x86 <christian.gg.x86@gmail.com>
2022-07-20 13:58:39 +0200

    a

--- misc/restaurante.sql
+++ misc/restaurante.sql
@@ -3,10 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-07-2022 a las 14:03:42
+-- Tiempo de generación: 20-07-2022 a las 13:48:40
 -- Versión del servidor: 10.4.19-MariaDB
 -- Versión de PHP: 8.0.7
-
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -39,14 +38,48 @@ CREATE TABLE `carta` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `carta_seccion`
+--
+
+CREATE TABLE `carta_seccion` (
+  `id_carta` int(11) NOT NULL,
+  `id_seccion` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `formato`
+--
+
+CREATE TABLE `formato` (
+  `id_formato` int(11) NOT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `precio` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `lineas_carta`
+--
+
+CREATE TABLE `lineas_carta` (
+  `id_plato` int(11) NOT NULL,
+  `id_formato` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `plato`
 --
 
 CREATE TABLE `plato` (
   `id_plato` int(11) NOT NULL,
   `nombre` varchar(20) NOT NULL,
-  `descripcion` varchar(200) NOT NULL,
-  `id_seccion` int(11) NOT NULL
+  `descripcion` varchar(200) DEFAULT NULL,
+  `id_seccion` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -69,6 +102,26 @@ CREATE TABLE `seccion` (
 --
 ALTER TABLE `carta`
   ADD PRIMARY KEY (`id_carta`);
+
+--
+-- Indices de la tabla `carta_seccion`
+--
+ALTER TABLE `carta_seccion`
+  ADD PRIMARY KEY (`id_carta`,`id_seccion`),
+  ADD KEY `id_seccion` (`id_seccion`);
+
+--
+-- Indices de la tabla `formato`
+--
+ALTER TABLE `formato`
+  ADD PRIMARY KEY (`id_formato`);
+
+--
+-- Indices de la tabla `lineas_carta`
+--
+ALTER TABLE `lineas_carta`
+  ADD PRIMARY KEY (`id_plato`,`id_formato`),
+  ADD KEY `id_formato` (`id_formato`);
 
 --
 -- Indices de la tabla `plato`
@@ -104,10 +157,30 @@ ALTER TABLE `plato`
 --
 ALTER TABLE `seccion`
   MODIFY `id_seccion` int(11) NOT NULL AUTO_INCREMENT;
+  
+--
+-- AUTO_INCREMENT de la tabla `formato`
+--
+ALTER TABLE `formato`
+  MODIFY `id_formato` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `carta_seccion`
+--
+ALTER TABLE `carta_seccion`
+  ADD CONSTRAINT `carta_seccion_ibfk_1` FOREIGN KEY (`id_carta`) REFERENCES `carta` (`id_carta`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `carta_seccion_ibfk_2` FOREIGN KEY (`id_seccion`) REFERENCES `seccion` (`id_seccion`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `lineas_carta`
+--
+ALTER TABLE `lineas_carta`
+  ADD CONSTRAINT `lineas_carta_ibfk_1` FOREIGN KEY (`id_plato`) REFERENCES `plato` (`id_plato`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `lineas_carta_ibfk_2` FOREIGN KEY (`id_formato`) REFERENCES `formato` (`id_formato`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `plato`
