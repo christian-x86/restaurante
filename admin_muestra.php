e70b1dcede11d96c4a13d7b0ae4c43496f584d41
christian-x86 <christian.gg.x86@gmail.com>
2022-08-02 14:27:31 +0200

    a

--- admin_muestra.php
+++ admin_muestra.php
@@ -30,14 +30,22 @@
   if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
   }
-  $sql = "SELECT id_carta, nombre FROM carta";
+  // $sql = "SELECT id_carta, nombre FROM carta";
+  $sql = "SELECT carta.nombre AS `Carta`, seccion.nombre AS `Sección`, plato.nombre AS `Nombre del plato`, formato.nombre AS `Formato`, lineas_carta.precio
+  FROM carta
+  JOIN seccion ON carta.id_carta = seccion.id_carta
+  JOIN plato_seccion ON seccion.id_seccion = plato_seccion.id_seccion
+  JOIN plato ON plato_seccion.id_plato=plato.id_plato
+  JOIN lineas_carta ON plato.id_plato=lineas_carta.id_plato
+  JOIN formato ON lineas_carta.id_formato=formato.id_formato
+  ORDER BY carta.id_carta, seccion.id_seccion, plato.nombre, formato.id_formato;";
   $result = $conn->query($sql);
   $arr=[];
   
   if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-      $arr+= [$row["id_carta"]=>$row["nombre"]];
+      array_push($arr, $row);
     }
   } else {
     echo "0 results";
