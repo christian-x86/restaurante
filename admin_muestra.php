4153b1fca36a39d053866ac5475cf853729461a0
christian-x86 <christian.gg.x86@gmail.com>
2022-08-04 11:58:27 +0200

    a

--- admin_muestra.php
+++ admin_muestra.php
@@ -9,53 +9,64 @@
   <title>Form Admin</title>
 </head>
 <body>
+  
 
   <?php
-  $servername = "localhost";
-  $username = "root";
-  $password = "";
-  $dbname = "restaurante2";
+  include("conexion.php");
   
-  // Create connection
-  $conn = new mysqli($servername, $username, $password, $dbname);
-  // Check connection
-  if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-  }
   $sql = "SELECT id_carta, nombre FROM carta";
-  /*
-  $sql = "SELECT carta.nombre AS `Carta`, seccion.nombre AS `Sección`, plato.nombre AS `Nombre del plato`, formato.nombre AS `Formato`, lineas_carta.precio
+  
+  
+  
+  $result = $conn->query($sql);
+
+?>
+  <form action="" method="post" id="form_carta">
+    <select name="carta" id="carta">
+
+      <?php
+      if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+          // array_push($arr, $row);
+          echo "<option value='".$row["id_carta"]."' id='select'>".$row["nombre"]."</option>";
+        }
+      } else {
+        echo "0 results";
+      }
+      $conn->close();
+
+      
+      ?>
+    </select>
+    <input type="submit" value="Enviar">
+  </form>
+  <?php
+  include("conexion.php");
+  $sql2 = "SELECT carta.nombre AS `Carta`, seccion.nombre AS `Sección`, plato.nombre AS `Nombre del plato`, formato.nombre AS `Formato`, lineas_carta.precio
   FROM carta
   JOIN seccion ON carta.id_carta = seccion.id_carta
   JOIN plato_seccion ON seccion.id_seccion = plato_seccion.id_seccion
   JOIN plato ON plato_seccion.id_plato=plato.id_plato
   JOIN lineas_carta ON plato.id_plato=lineas_carta.id_plato
   JOIN formato ON lineas_carta.id_formato=formato.id_formato
+  WHERE carta.id_carta=".$_POST["carta"]."
   ORDER BY carta.id_carta, seccion.id_seccion, plato.nombre, formato.id_formato;";
-  */
-  $result = $conn->query($sql);
-  $arr=[];
 
-
-?>
-<form action="" method="post" id="form_carta">
-  <select name="carta" id="carta">
-
-    <?php
-    if ($result->num_rows > 0) {
-      // output data of each row
-      while($row = $result->fetch_assoc()) {
-        // array_push($arr, $row);
-        echo "<option value='".$row["id_carta"]."' id='select'>".$row["nombre"]."</option>";
-      }
-    } else {
-      echo "0 results";
+  $result = $conn->query($sql2);
+  
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      // array_push($arr, $row);
+      var_dump($row);
+      echo "<br>";
     }
-    $conn->close();
-
-    
-    ?>
-  </select>
-</form>
+  } else {
+    echo "0 results";
+  }
+  $conn->close();
+  
+  ?>
 </body>
 </html>
