4eb1b3887dd5ab6963f0e1e62b23fadc7125ea89
christian-x86 <108787839+christian-x86@users.noreply.github.com>
2022-12-05 11:57:28 +0100

    Delete misc/windows_batch directory

--- misc/windows_batch/script_move.bat
+++
@@ -1,5 +0,0 @@
-set actual=W
-
-for /f "tokens=*" %%G in ('dir /b /s /a:d "C:\Users\%USERNAME%\Documents\a\*%actual%*"') do move "%%G" "C:\Users\%USERNAME%\Documents\b"
-
-pause
--- misc/windows_batch/script_move2.bat
+++
@@ -1,7 +0,0 @@
-
-for /f "usebackq tokens=* delims=," %%a in ("C:\Users\CRISTIAN GARCIA\Documents\lunes.csv") do (
-      echo %%a
-      for /f "tokens=*" %%G in ('dir /b /s /a:d "C:\Users\%USERNAME%\Documents\a\*%%a*"') do move "%%G" "C:\Users\%USERNAME%\Documents\b"
-       )
-
-pause
